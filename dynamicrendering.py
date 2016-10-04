fd8853a11c7e5140a0590b66489ffbdcff62a3d1
atyamsriharsha <atyamsriharsha@gmail.com>
2016-10-05 03:10:07 +0530

    Dynamic Rendering Done Perfectly

--- dynamicrendering.py
+++ dynamicrendering.py
@@ -6,26 +6,25 @@ cap = cv2.VideoCapture("output.avi")
 fourcc = cv2.cv.CV_FOURCC(*'XVID')
 out = cv2.VideoWriter('output2.avi',fourcc, 20.0, (640,480))
 
-fo = open("input.txt","r").read()
-# for x in fo:
-#     print x
-#     print "----------\n"
-# #lines = fo.readlines()
+fo = open("input.txt","rw+")
 lineNumber = 0
+line = fo.readline()
+framecount = 0
 while cap.isOpened():
     ret,frame = cap.read()
     if ret==True:
-        #startx,starty,endx,endy,threshold = map(int,lines[lineNumber].split())
+        startx,starty,endx,endy,threshold = map(int,line.split())
         frame = frame[startx:endx,starty:endy]
         cv2.imshow('frame',frame)
         if cv2.waitKey(1) and 0xFF ==ord('q'):
             break
         framecount = framecount+1
         if framecount%threshold==0:
-            lineNumber = lineNumber+5
+            line = fo.readline()
     else:
         break
 
+fo.close()
 cap.release()
 out.release()
 cv2.destroyAllWindows()
